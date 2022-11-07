b5c9f3b50f611a18dca20e34de28ddf519f0a7de
Kawan <kawan.ferreira@sptech.school>
2022-11-07 20:20:20 -0300

    Inserindo java para inicialização

--- scriptDecisao.sh
+++ scriptDecisao.sh
@@ -3,6 +3,9 @@ java -version
 if [ $? -eq 0 ]
 then
 echo \"java instalado\"
+git clone https://github.com/Grupo-5-2-Semestre/Trackio-JAR-CLI.git
+cd Trackio-JAR-CLI
+java -jar JARCLI.jar
 else
 echo \"java não instalado\"
 echo \"gostaria de instalar o java? s/n \"
@@ -13,13 +16,24 @@ echo \"voce escolheu instalar o java\"
 echo \"instalando repositorio\"
 sleep 2
 sudo apt update -y
-sudo apt install default-jdk
+sudo apt install default-jdk -y
 clear
 echo \"Atualizando repositorio\"
 sleep 2
 sudo apt update -y
 clear
+git clone https://github.com/Grupo-5-2-Semestre/Trackio-JAR-CLI.git
+cd Trackio-JAR-CLI.git
+java -jar JARCLI.jar
 else echo \"você escolheu não instalar\"
+echo \"gostaria de iniciar? s/n \"
+read init
+if [ \"$init\" == \"s\" ]
+then
+git clone https://github.com/Grupo-5-2-Semestre/Trackio-JAR-CLI.git
+cd Trackio-JAR-CLI.git
+java -jar JARCLI.jar
+fi
 fi
 fi
  
